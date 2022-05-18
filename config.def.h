2b87ed49075c51687f7b06fb11aeba12a65b52f0
Torskaren <101107437+Torskaren@users.noreply.github.com>
2022-05-17 21:06:41 -0400

    Add files via upload
    Includes a variety of status monitors for the following: Network transfer speed, volume level, battery percentage, RAM usage, and CPU usage.

--- config.def.h
+++ config.def.h
@@ -4,7 +4,7 @@
 const unsigned int interval = 1000;
 
 /* text to show if no value can be retrieved */
-static const char unknown_str[] = "n/a";
+static const char unknown_str[] = "nil";
 
 /* maximum output string length */
 #define MAXLEN 2048
@@ -65,8 +65,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ vol_perc, "{   VOL   %s | ", 	"/dev/mixer"},
-	{ battery_perc, "BAT   %s%% | ", "BAT0"	},
-	{ ram_perc, "RAM   %s%% | ", NULL	},
-	{ cpu_perc, "CPU   %s%% ", NULL	}
+	{ netspeed_rx, "( NET: %sB/s | ", 	"wlp3s0"},
+	{ run_command, "VOL:%4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ battery_perc, "BAT: %s%% | ", "BAT0"	},
+	{ ram_perc, "RAM: %s%% | ", NULL	},
+	{ cpu_perc, "CPU: %s%% ", NULL	}
 };
